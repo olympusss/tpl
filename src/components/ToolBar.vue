d833e944dbda3434d915651078b76cb20a5a6fb4
olympusss <rasulov.titu@gmail.com>
2023-03-12 05:25:44 +0500

    commit

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-sheet color="#212135">
+    <v-container 
+      class="pa-0" 
+      height="40"
+    >
+      <v-app-bar 
+        dense 
+        flat 
+        color="#212135" 
+        dark 
+        height="40"
+      >
+        <v-spacer></v-spacer>
+        <v-toolbar-items 
+          style="margin-right: 45px;"
+        >
+          <v-toolbar-title
+            class="btn mt-1 ml-12" 
+            height="40"
+            v-for="link in links"
+            :key="link.title"
+          >
+            <router-link 
+              to="/" 
+              tag="span" 
+              class="pointer" 
+              style="color: #EEEEEE; 
+              font-size: small;"
+            >
+              {{ link.title }}
+              <v-span 
+                style="color: #cb3131; 
+                margin-left: 3px"
+              >
+                {{ link.count }}
+              </v-span>
+            </router-link>
+          </v-toolbar-title>
+        </v-toolbar-items>
+        <v-divider 
+          vertical 
+          class="mx-0"
+        >
+        </v-divider>
+        <v-btn
+          color="#212135"
+          :v-bind="buttonName"
+          small
+          flat
+          depressed
+          tile
+          height="40"
+          @click="expand = !expand"
+        >
+          {{ buttonName }}
+        </v-btn>
+        <v-divider 
+          vertical 
+          class="ml-0"
+        ></v-divider>
+        
+        <v-menu
+          transition="slide-y-transition"
+          v-model="expand"
+          position-x="1151"
+          position-y="40"
+          tile
+          min-width="100"
+        >
+          <v-list class="py-0" 
+            color="#212135"
+            dark
+          >
+            <v-list-item
+              v-for="lang in languages"
+              :key="lang.name"
+              @click="changeButtonName(lang.abbr)"
+            >
+              <v-list-item-title
+                style="font-size: smaller;"
+                
+              >
+                {{ lang.name }}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+    </v-container>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    buttonName: 'EN',
+    expand: false,
+    links: [
+      { title: "Bildirişler", count: 99 },
+      { title: "Bazar", count: 99 },
+      { title: "Habarlar & Media", count: 99 },
+    ],
+    languages: [
+      { name: 'English', abbr: 'EN' },
+      { name: 'Русский', abbr: 'RU'  },
+      { name: 'Türkmen', abbr: 'TM'  },
+    ]
+  }),
+  methods: {
+    changeButtonName(lang) {
+      this.buttonName = lang
+      console.log(lang)
+    }
+  },
+};
+</script>
+
+<style>
+.btn {
+  text-transform: unset !important;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,136 @@
+<template>
+  <v-app 
+    id="inspire" 
+    :style="{background: $vuetify.theme.themes.dark.background}"
+  >
+    <ToolBar />
+    <v-container 
+      class="pa-0 mt-2" 
+      height="60" 
+      style="background-color: red;"
+    >
+      <v-app-bar
+        dense
+        flat
+        height="60"
+        color="#EEEEEE"
+      >
+        <v-toolbar-title>
+          <div>
+            <b 
+              class="mytitle"
+            >
+              TELEKEÇI
+            </b>
+          </div> 
+          <div 
+            class="mt-n3"
+          >
+            <b 
+              class="mytitle"
+            >
+              PLATFORMA
+            </b>
+          </div>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        
+        
+        <v-toolbar-items>
+          <v-toolbar-title
+            style="align-self: center;"
+            class="mx-4"
+          >
+            <router-link 
+              to="/" 
+              tag="span" 
+              class="pointer"
+            >
+              Bölümler
+              <v-icon 
+                class="ml-n1"
+              >
+                keyboard_arrow_down
+              </v-icon>
+            </router-link>
+          </v-toolbar-title>
+
+          <v-toolbar-title
+            style="align-self: center;"
+            class="mx-4"
+          >
+            <router-link to="/" tag="span" class="pointer">Halanlarym</router-link>
+          </v-toolbar-title>
+
+          <v-toolbar-title
+            style="align-self: center;"
+            class="mx-4"
+          >
+            <router-link to="/" tag="span" class="pointer">Meniň bilidirişlerim</router-link>
+          </v-toolbar-title>
+        </v-toolbar-items>
+
+
+        <v-btn 
+          icon
+          tile
+          flat
+          style="background-color: white;"
+          class="py-0 mx-5"
+          height="40"
+          width="40"
+        >
+          <v-icon size="30" color="#212135">location_on</v-icon>
+        </v-btn>
+
+        <v-btn 
+          icon
+          tile
+          flat
+          style="background-color: white;"
+          class="py-0 mr-0"
+          height="40"
+          width="40"
+        >
+          <v-icon size="30" color="#212135">perm_identity</v-icon>
+        </v-btn>
+
+      </v-app-bar>
+    </v-container>
+    <FooterComponent />
+  </v-app>
+</template>
+
+<script>
+  import ToolBar from '../components/ToolBar'
+  import FooterComponent from '../components/FooterComponent'
+
+  export default {
+    data: () => ({
+      toolbars: [
+        { title: 'Bölümler' },
+        { title: 'Halanlarym' },
+        { title: 'Meniň bilidirişlerim' },
+      ]
+    }),
+    components: {
+      ToolBar,
+      FooterComponent
+    },
+  }
+</script>
+
+<style>
+.mytitle {
+  font-size: 20px !important;
+  font-weight: 800;
+  color: #212135;
+}
+
+.pointer {
+  cursor: pointer;
+  font-size: medium;
+  font-weight: 700;
+  color: #212135;
+}
+</style>
